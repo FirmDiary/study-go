2237230d2f7bbec1da5b568ac046f67db4a84754
FirmDiary <zhaozw5@163.com>
2020-12-20 15:46:24 +0800

    Hello, World

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,11 +2,24 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-    got := Hello("Azal")
-    want := "Hello, Azal"
+func TestHello(t *testing.T) {
 
-    if got != want {
-        t.Errorf("got '%q' want '%q'", got, want)
-    }
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Azal")
+		want := "Hello, Azal"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
