8b3ad5be48749bb4595a59a5e4f9ab4f1cef95bb
FirmDiary <zhaozw5@163.com>
2020-12-20 18:37:48 +0800

    range

--- array/sum.go
+++ array/sum.go
@@ -2,8 +2,14 @@ package array
 
 func Sum(numbers [5]int) (sum int) {
 	sum = 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	//for i := 0; i < 5; i++ {
+	//	sum += numbers[i]
+	//}
+
+	// range 迭代数组 每次返回索引 与 值
+	// 使用 _ 空白字符串 来忽略索引
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
