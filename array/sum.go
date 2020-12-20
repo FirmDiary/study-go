4eda42d511e7db4520e7aec6efdcf869c55bcf94
FirmDiary <zhaozw5@163.com>
2020-12-20 18:46:56 +0800

    splice, go test --cover

--- array/sum.go
+++ array/sum.go
@@ -1,6 +1,6 @@
 package array
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	sum = 0
 	//for i := 0; i < 5; i++ {
 	//	sum += numbers[i]
--- array/sum_test.go
+++ array/sum_test.go
@@ -3,12 +3,30 @@ package array
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-		//%v 默认输入格式 适合展示数组
-	}
+	//t.Run("Collection of 5 numbers", func(t *testing.T) {
+	//    numbers := [5]int{1, 2, 3, 4, 5}
+	//
+	//    got := Sum(numbers)
+	//    want := 15
+	//
+	//    if got != want {
+	//        t.Errorf("got %d want %d given, %v", got, want, numbers)
+	//        //%v 默认输入格式 适合展示数组
+	//    }
+	//})
+
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
+
+//go test --cover 查看测试覆盖率
