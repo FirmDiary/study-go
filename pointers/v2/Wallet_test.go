f84a43a477dc17005e654b17a9fb493c8df21800
Azal <zhaozw5@163.com>
2020-12-25 20:37:05 +0800

    type from OriginType, rewrite OriginType's func

---
+++ pointers/v2/Wallet_test.go
@@ -0,0 +1,21 @@
+package v1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+
+	fmt.Println("address of balance in test is", &wallet.balance)
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
