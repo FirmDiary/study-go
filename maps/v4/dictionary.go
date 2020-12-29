c5c9c4afa5565f6b83984c16ab2b8ab7273ef713
FirmDiary <zhaozw5@163.com>
2020-12-29 22:32:21 +0800

    maps, CURD, handle error, test

---
+++ maps/v4/dictionary.go
@@ -0,0 +1,67 @@
+package v1
+
+const (
+	ErrNotFound      = DictionaryErr("could not find the word you were looking for")
+	ErrKeyExists     = DictionaryErr("cannot add word because it already exists")
+	ErrorKeyNotExist = DictionaryErr("cannot update word because it already exists")
+)
+
+//我们将错误声明为常量，这需要我们创建自己的 DictionaryErr 类型来实现 error 接口
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+//我们现在有很好的方法来搜索字典。但是，我们无法在字典中添加新单词。
+func (d Dictionary) Search(word string) (string, error) {
+	// map 查找的有趣特性。它可以返回两个值。第二个值是一个布尔值，表示是否成功找到 key
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(key string, word string) error {
+	//Map 有一个有趣的特性，不使用指针传递你就可以修改它们。这是因为 map 是引用类型
+	//它底层的数据结构是 hash table 或 hash map
+
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = word
+	case nil:
+		return ErrKeyExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(key, word string) error {
+
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		return ErrorKeyNotExist
+	case nil:
+		d[key] = word
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(key string) {
+	//Go 的 map 有一个内置函数 delete。它需要两个参数。
+	//第一个是这个 map，第二个是要删除的键。
+	delete(d, key)
+}
---
+++ maps/v4/dictionary_test.go
@@ -0,0 +1,109 @@
+package v1
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertStrings(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+
+	t.Run("new key", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		key := "test"
+		word := "this is just a test"
+
+		dictionary.Add(key, word)
+
+		assertDefinition(t, dictionary, key, word)
+	})
+
+	t.Run("exist key", func(t *testing.T) {
+		key := "test"
+		word := "this is just a test"
+
+		dictionary := Dictionary{key: word}
+
+		err := dictionary.Add(key, "new test")
+
+		assertError(t, err, ErrKeyExists)
+		assertDefinition(t, dictionary, key, word)
+	})
+
+	t.Run("update key exist", func(t *testing.T) {
+		key := "test"
+		word := "this is a test"
+		newWord := "hello world"
+
+		dictionary := Dictionary{key: word}
+
+		dictionary.Update(key, newWord)
+
+		assertDefinition(t, dictionary, key, newWord)
+	})
+
+	t.Run("update key not exist", func(t *testing.T) {
+
+		key := "test"
+		word := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(key, word)
+
+		assertError(t, err, ErrorKeyNotExist)
+	})
+
+	t.Run("delete key exist", func(t *testing.T) {
+		key := "test"
+		word := "this is just a test"
+		dictionary := Dictionary{key: word}
+
+		dictionary.Delete(key)
+
+		_, err := dictionary.Search(key)
+		if err != ErrNotFound {
+			t.Errorf("Expected '%s' to be deleted", word)
+		}
+	})
+
+}
+
+func assertError(t *testing.T, got error, want error) {
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got '%s', but want '%s'", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, key, word string) {
+	t.Helper()
+
+	got, err := dictionary.Search(key)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if word != got {
+		t.Errorf("got '%s' but want '%s'", got, word)
+	}
+}
+
+func assertStrings(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got '%s' want '%s' given, '%s'", got, want, "test")
+	}
+
+}
