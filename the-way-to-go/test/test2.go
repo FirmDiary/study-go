7f3a1e590ad161fd5a311401505620af21715774
Azal <zhaozw5@163.com>
2021-01-19 18:29:55 +0800

    类型与运算符

--- the-way-to-go/test/test2.go
+++ the-way-to-go/test/test2.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
+//func main() {
+//	var a int
+//	var b int32
+//	a = 15
+//	b = a + a // 编译错误  cannot use a + a (type int) as type int32 in assignment
+//	b = b + 5 // 常量是可以隐士转换的, 因为 5 是常量，所以可以通过编译
+//}
+
 func main() {
-	var a int
-	var b int32
-	a = 15
-	b = a + a // 编译错误
-	b = b + 5 // 因为 5 是常量，所以可以通过编译
+	var n int16 = 34
+	var m int32
+
+	m = int32(n) //必须通过显示转换
+
+	fmt.Printf("32 bit int is: %d\n", m)
+	fmt.Printf("16 bit int is: %d\n", n)
 }
