fbcc1252811b3130ab3970cb17a5b95209664d9f
Azal <zhaozw5@163.com>
2021-02-08 15:37:39 +0800

    协程 go标识符

---
+++ the-way-to-go/test/goroutine/test1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//协程 go标识符
+func main() {
+	fmt.Println("In main()")
+
+	//协程并行运行
+	go longWait()
+	go shortWait()
+	fmt.Println("About to sleep in main()")
+	// sleep works with a Duration in nanoseconds (ns) !
+	time.Sleep(10 * 1e9)
+	fmt.Println("At the end of main()")
+
+	//Go协程是并行的，是通过通道来通信的
+}
+
+func longWait() {
+	fmt.Println("Beginning longWait()")
+	time.Sleep(5 * 1e9) // sleep for 5 seconds
+	fmt.Println("End of longWait()")
+}
+
+func shortWait() {
+	fmt.Println("Beginning shortWait()")
+	time.Sleep(2 * 1e9) // sleep for 2 seconds
+	fmt.Println("End of shortWait()")
+}
