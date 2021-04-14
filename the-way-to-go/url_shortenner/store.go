0fce581d017747bb5a42ba851e3d6ae70a71e134
赵泽武 <zhaozw5@163.com>
2021-04-14 11:42:52 +0800

    feat: url_shortenner

--- the-way-to-go/url_shortenner/store.go
+++ the-way-to-go/url_shortenner/store.go
@@ -7,24 +7,22 @@ type URLStore struct {
 	mu   sync.RWMutex
 }
 
-func NewUrlStore() *URLStore {
+func NewURLStore() *URLStore {
 	return &URLStore{urls: make(map[string]string)}
 }
 
 func (s *URLStore) Get(key string) string {
 	s.mu.RLock()
-	defer s.mu.Unlock()
+	defer s.mu.RUnlock()
 	return s.urls[key]
 }
 
-func (s *URLStore) Set(key string, url string) bool {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
+func (s *URLStore) Set(key, url string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	if _, present := s.urls[key]; present {
 		return false
 	}
-
 	s.urls[key] = url
 	return true
 }
@@ -36,8 +34,12 @@ func (s *URLStore) Count() int {
 }
 
 func (s *URLStore) Put(url string) string {
-
 	for {
-		//key := getKey
+		key := genKey(s.Count()) // generate the short URL
+		if ok := s.Set(key, url); ok {
+			return key
+		}
 	}
+	// shouldn't get here
+	return ""
 }
